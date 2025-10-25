e7bb0d044e1c166417f5d28b2154568866045a1c
koshkakateyka <koneika336@gmail.com>
2025-10-25 19:19:16 +0300

    add for open the logic for opening just a file

--- src/Open.java
+++ src/Open.java
@@ -1,7 +1,18 @@
 import Impl.OpenImpl;
 
+import java.io.*;
+
 public class Open implements OpenImpl {
     @Override
     public void execute(String filePath) {
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            int c; while((c = fileReader.read()) != -1){
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
